03bbadf5e66b01b371132117d52b33747aace891
Floed1 <daniel_fessing@gmx.de>
2025-03-27 15:45:42 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(() => {
+        document.querySelector(".splash-screen").style.display = "none";
+        document.querySelector(".main-content").style.display = "block";
+    }, 3069);
+});
